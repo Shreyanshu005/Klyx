790f463af4d1bee1435c1ddbd457ba59269d2504
shreyanshu005 <shreyanshu005@gmail.com>
2026-04-17 15:19:39 +0530

    Initial Commit

---
+++ Klyx/KlyxApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlyxApp.swift
+//  Klyx
+//
+//  Created by Shreyanshu on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct KlyxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KlyxTests/KlyxTests.swift
@@ -0,0 +1,17 @@
+//
+//  KlyxTests.swift
+//  KlyxTests
+//
+//  Created by Shreyanshu on 17/04/26.
+//
+
+import Testing
+@testable import Klyx
+
+struct KlyxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
